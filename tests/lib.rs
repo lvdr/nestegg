bbfeb19546133541e1a89c2b1f53dd489c195456
VeeDeltaVee <45564258+VeeDeltaVee@users.noreply.github.com>
2019-12-10 21:42:10 +0530

    Redid smoke test

---
+++ tests/lib.rs
@@ -0,0 +1,13 @@
+use nestegg::ComputerState;
+
+#[test]
+fn smoketest() {
+    let initial_memory = vec![0xEA; 10];
+    let expected_memory = initial_memory.clone();
+
+    let final_memory = ComputerState::initialize_from_image(initial_memory)
+        .multiple_steps(10)
+        .memory;
+
+    assert_eq!(final_memory, expected_memory);
+}
