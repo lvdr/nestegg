24a21165fcb57bbe1918837988442b1c4e73e589
VeeDeltaVee <veedeltavee@gmail.com>
2022-06-17 22:31:03 -0700

    Refactored tests heavily

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -14,37 +14,63 @@ pub struct Statement<'a> {
 
 #[derive(Debug, PartialEq)]
 pub enum Expression {
-    Decimal(u16),
+    Number(u16),
+}
+
+fn ensure_tokens_available(tokens: &[Token], n: usize) -> Result<(), &'static str> {
+    if tokens.len() < n {
+        Err("Unexpected end of program")
+    } else {
+        Ok(())
+    }
 }
 
 fn munch_label<'a>(tokens: &'a [Token]) -> Result<(&'a [Token<'a>], &'a str), &'static str> {
-    if tokens.len() >= 2 && tokens[0].name == "Label" && tokens[1].name == "Colon" {
-        Ok((&tokens[2..], tokens[0].text))
-    } else {
-        Err("Didn't find a label")
+    ensure_tokens_available(tokens, 2)?;
+
+    match tokens {
+        [Token {name: "Label", ..}, Token {name: "Colon", ..}] =>
+            Ok((&tokens[2..], tokens[0].text)),
+        _ =>
+            Err("Didn't find a label"),
     }
 }
 
 fn munch_operation<'a>(tokens: &'a [Token]) -> Result<(&'a [Token<'a>], Operation), &'static str> {
-    if tokens.len() == 0 {
-        Err("Unexpected end of program")
+    ensure_tokens_available(tokens, 1)?;
+    Ok((&tokens[1..], Operation::from_str(&tokens[0].text.to_uppercase())?))
+}
+
+
+fn parse_decimal_number<'a>(tokens: &'a [Token]) -> Result<(&'a [Token<'a>], u16), &'static str> {
+    ensure_tokens_available(tokens, 1);
+
+    if let Ok(value) = tokens[0].text.parse::<u16>() {
+        Ok((&tokens[1..], value))
     } else {
-        Ok((&tokens[1..], Operation::from_str(&tokens[0].text.to_uppercase())?))
+        Err("Expected decimal number")
+    }
+}
+
+fn parse_hex_number<'a>(tokens: &'a [Token]) -> Result<(&'a [Token<'a>], u16), &'static str> {
+    ensure_tokens_available(tokens, 2);
+
+    if tokens[0].name == "Hex" {
+        if let Ok(value) = u16::from_str_radix(tokens[1].text, 16) {
+            Ok((&tokens[2..], value))
+        } else {
+            Err("Expected decimal number")
+        }
+    } else {
+        Err("Couldn't find hex number")
     }
 }
 
 fn munch_expression<'a>(tokens: &'a [Token]) -> Result<(&'a [Token<'a>], Expression), &'static str> {
-    if tokens.len() >= 1 && tokens[0].name == "Number" {
-        if let Ok(value) = tokens[0].text.parse::<u16>() {
-            Ok((
-                &tokens[1..],
-                Expression::Decimal(value)
-            ))
-        } else {
-                Err("Expected decimal number")
-            }
-    } else if tokens.len() == 0 {
-        Err("Unexpected end of program")
+    if let Ok((new_tokens, value)) = parse_decimal_number(tokens) {
+        Ok((new_tokens, Expression::Number(value)))
+    } else if let Ok((new_tokens, value)) = parse_hex_number(tokens) {
+        Ok((new_tokens, Expression::Number(value)))
     } else {
         Err("Didn't find an expression")
     }
@@ -66,7 +92,7 @@ fn parse_statement<'a>(tokens: &'a [Token<'a>]) -> Result<(&'a [Token<'a>], Stat
         Statement {
             label,
             operation,
-            expression: Expression::Decimal(0),
+            expression: Expression::Number(0),
         }
     ))
 }
@@ -86,7 +112,22 @@ pub fn parse<'a>(tokens: &'a [Token<'a>]) -> Result<Program<'a>, &'static str> {
 
 #[cfg(test)]
 mod test {
+    use std::fmt::Debug;
+
     use super::*;
+
+    /// Asserts that `parser(tokens)` returns `expected_value` and consumes
+    /// `expected_consumed_tokens` values from `tokens`
+    fn assert_parsing_tokens_with_func_gives_and_consumes<'a, T: PartialEq + Debug, E: Debug>(
+        tokens: &'a [Token],
+        parser: fn (&'a [Token]) -> Result<(&'a [Token<'a>], T), E>,
+        expected_value: T,
+        expected_consumed_tokens: usize
+    ) {
+        let (returned_tokens, returned_val) = parser(&tokens).unwrap();
+        assert_eq!(returned_val, expected_value);
+        assert_eq!(returned_tokens.len(), tokens.len() - expected_consumed_tokens)
+    }
 
     mod describe_munch_label {
         use super::*;
@@ -103,8 +144,7 @@ mod test {
                 },
             ];
 
-            let (_, label) = munch_label(&mock_tokens).unwrap();
-            assert_eq!(label, "GotoLabel");
+            assert_parsing_tokens_with_func_gives_and_consumes(&mock_tokens, munch_label, "GotoLabel", 2);
         }
     }
 
@@ -120,16 +160,32 @@ mod test {
                 },
             ];
 
-            let (_, op) = munch_operation(&mock_tokens).unwrap();
-            assert_eq!(op, Operation::ADC);
+            assert_parsing_tokens_with_func_gives_and_consumes(&mock_tokens, munch_operation, Operation::ADC, 1);
         }
     }
 
     mod describe_munch_expression {
         use super::*;
 
+        mod describe_parse_decimal_number {
+            use super::*;
+
+            #[test]
+            fn it_works_correctly() {
+                let mock_tokens = vec![
+                    Token {
+                        name: "Number",
+                        text: "5312",
+                    },
+                ];
+
+                assert_parsing_tokens_with_func_gives_and_consumes(&mock_tokens, parse_decimal_number, 5312, 1);
+            }
+        }
+
+
         #[test]
-        fn it_works_correctly_for_decimals() {
+        fn it_works_correctly_for_decimal_numbers() {
             let mock_tokens = vec![
                 Token {
                     name: "Number",
@@ -137,8 +193,7 @@ mod test {
                 },
             ];
 
-            let (_, expr) = munch_expression(&mock_tokens).unwrap();
-            assert_eq!(expr, Expression::Decimal(5312));
+            assert_parsing_tokens_with_func_gives_and_consumes(&mock_tokens, munch_expression, Expression::Number(5312), 1);
         }
     }
 }
